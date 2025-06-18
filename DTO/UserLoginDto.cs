bdd3b89a80897572402cb24e94989e316fcb878d
Foreign1923 <orkunozdemir25@gmail.com>
2025-06-18 14:30:19 +0300

    folder structure düzeltildi.

--- DTO/UserLoginDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SocialNetworkAPI.DTO
-{
-    public class UserLoginDto
-    {
-        public string Email { get; set; } = null!;
-        public string Password { get; set; } = null!;
-    }
-
-}
